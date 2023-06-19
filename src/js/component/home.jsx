847b279fbf764c2fee3432b8e00d06af869e6edb
Eduardo Lopes <129979638+Edulopesp@users.noreply.github.com>
2023-06-19 16:52:42 +0000

    full project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import '../../../src/styles/index.css'; // Replace with the actual path to your stylesheet file
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+
+
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+export const Home = () => {
 
-export default Home;
+	const [count, setCount] = useState(0);
+
+  	useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((old) => old + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className="count-container" style={{ fontFamily: 'Press Start 2P' }}>
+		<div className="clock">
+		<FontAwesomeIcon icon={faClock} style={{ color: '#ffffff' }} />
+		</div>
+      {Array.from(String(count).padStart(6, '0')).map((digit, index) => (
+        <div key={index} className="number">{digit}</div>
+        ))}
+    </div>
+  	);
+};
